e3a7e88bf8b565f6d3886fc6e4a366985b1feac7
Mevan Samaratunga <mevansam@gmail.com>
2016-09-08 10:08:59 +0400

    updated config to use git repo directly instead of downloading zip from git repo

--- scripts/run-pcf-config.sh
+++ scripts/run-pcf-config.sh
@@ -5,10 +5,8 @@ set -e
 
 # Environment variables
 #
-# DOWNLOADS_DIR
-# SCRIPTS_DIR
+# ROOT_DIR
 # CONFIG_DIR
-# BACKUP_DIR
 #
 # OPSMAN_HOST
 # OPSMAN_USER
@@ -18,9 +16,7 @@ set -e
 # PCF_CONFIG
 # LDAP_BIND_PASSWD (optional)
 
-if [ -z "$DOWNLOADS_DIR" ] ||
-    [ -z "$SCRIPTS_DIR" ] ||
-    [ -z "$BACKUP_DIR" ] ||
+if [ -z "$ROOT_DIR" ] ||
     [ -z "$CONFIG_DIR" ] ||
     [ -z "$OPSMAN_HOST" ] ||
     [ -z "$OPSMAN_USER" ] ||
@@ -33,13 +29,29 @@ if [ -z "$DOWNLOADS_DIR" ] ||
 fi
 
 run_job=1
-if [[ -e $DOWNLOADS_DIR/$PCF_CONFIG.zip.last ]]; then
-	set +e
-    diff $DOWNLOADS_DIR/$PCF_CONFIG.zip $DOWNLOADS_DIR/$PCF_CONFIG.zip.last
-    if [[ $? -ne 2 ]]; then
+if [[ ! -e $CONFIG_DIR/$PCF_CONFIG ]]; then
+    git clone $CONFIG_GIT_URL --branch master --single-branch $ROOT_DIR
+    pushd $ROOT_DIR
+    git checkout master
+    popd
+else
+    pushd $ROOT_DIR
+    git checkout master
+
+    LOCAL=$(git rev-parse @)
+    REMOTE=$(git rev-parse @{u})
+    BASE=$(git merge-base @ @{u})
+
+    if [ $LOCAL = $REMOTE ]; then
         run_job=0
+    elif [ $LOCAL = $BASE ]; then
+        git pull
+    else
+        echo "Unable to determine state of '$CONFIG_GIT_URL'."
+        exit 1
     fi
-    set -e
+
+    popd
 fi
 
 test_run=""
@@ -57,13 +69,13 @@ if [[ $run_job -eq 1 ]]; then
 	cd $CONFIG_DIR/$PCF_CONFIG
 	if [ -z "$OPSMAN_KEY" ]; then
 
-		$SCRIPTS_DIR/scripts/configure-ert.rb -o $OPSMAN_HOST \
+		echo $SCRIPTS_DIR/scripts/configure-ert.rb -o $OPSMAN_HOST \
 			-u "$OPSMAN_USER" \
 			-p "$OPSMAN_PASSWD" \
 			-w "$LDAP_BIND_PASSWD" \
             $test_run
 	else
-		$SCRIPTS_DIR/scripts/configure-ert.rb -o $OPSMAN_HOST \
+		echo $SCRIPTS_DIR/scripts/configure-ert.rb -o $OPSMAN_HOST \
 			-u "$OPSMAN_USER" \
 			-p "$OPSMAN_PASSWD" \
 			-w "$LDAP_BIND_PASSWD" \
@@ -71,11 +83,10 @@ if [[ $run_job -eq 1 ]]; then
             $test_run
 	fi
 
-    if [[ "$TEST_RUN" == "0" ]]; then
-        mv $DOWNLOADS_DIR/$PCF_CONFIG.zip $DOWNLOADS_DIR/$PCF_CONFIG.zip.last
+    if [[ "$TEST_RUN" == "1" ]]; then
+        # Force config to be re-cloned next run
+        rm -fr $CONFIG_DIR/$PCF_CONFIG
     fi
-else
-    rm $DOWNLOADS_DIR/$PCF_CONFIG.zip
 fi
 
 set +e
